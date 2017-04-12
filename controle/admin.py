1daad63368af29e4f4dfb08c5e5230272005ffe6
elizioalmeida <elizio.almeida@gmail.com>
2017-04-12 13:20:50 -0300

    segundo

--- controle/admin.py
+++ controle/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 
-from .models import CPod
+from .models import CPod, Escopo
 
 # Register your models here.
-admin.site.register(CPod)
+
+
+
+#class ChoiceInline(admin.StackedInline):
+
+class ChoiceInline(admin.TabularInline):
+    model = Escopo
+    extra = 3
+
+
+class CPodAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None,          {'fields':['nome_cp']}),
+            ('Cliente', {'fields':['cliente'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ( 'nome_cp', 'cliente', 'data_ini', 'data_fim')
+    list_filter = ['nome_cp']
+
+admin.site.register(CPod, CPodAdmin)
+
 
