2b533dcbf153a37cd610cd9dd670a33dfb3f966f
elizioalmeida <elizio.almeida@gmail.com>
2017-04-18 13:18:22 -0300

    estrutura da pagina inical

--- controle/views.py
+++ controle/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import CPod
 
 # Create your views here.
+
+def inicial(request):
+    cprod = CPod.objects.order_by('nome_cp')
+
+    return render(request, 'controle/inicial.html', {'cprod': cprod})
+
+
